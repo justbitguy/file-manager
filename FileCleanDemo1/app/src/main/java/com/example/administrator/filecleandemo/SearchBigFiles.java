08ca1e5f118eec5d475725bfe0e0c755b44ad182
zhangjinwei <3077663517@qq.com>
2016-08-24 15:37:03 +0800

    解决编译问题

--- FileCleanDemo1/app/src/main/java/com/example/administrator/filecleandemo/SearchBigFiles.java
+++ FileCleanDemo1/app/src/main/java/com/example/administrator/filecleandemo/SearchBigFiles.java
@@ -9,7 +9,7 @@ import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
